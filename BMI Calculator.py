43dbdc20d458b01432712b7ecd017529d26702f1
Vivek kumar Nayan <vivekkumarnayan2@gmail.com>
2022-11-25 20:18:28 +0530

    add commit

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,11 +1,9 @@
 height = input("Enter the value of hight in m : ")
 weight = input("Enter the value of weight in kg : ")
-
-
 height = float(height)
 weight = int(weight)
-
-
 bmi = float(weight)/float(height) ** 2 
 print(bmi)
 
+
+# bmi calculator
